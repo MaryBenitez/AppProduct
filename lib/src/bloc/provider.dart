476bbe5a7ea44e8e25c01609ce0a59b9a4ad4d9c
marisol <00094716@uca.edu.sv>
2020-02-24 14:12:44 -0600

    InheritedWidget

---
+++ lib/src/bloc/provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:formvalidation/src/bloc/login_bloc.dart';
+
+class Provider extends InheritedWidget {
+  final loginBloc = LoginBloc();
+
+  Provider({Key key, Widget child}) : super(key: key, child: child);
+
+  //Condicion: Al actualizarse debe notificar a sus hijos. El 99% de los casos es cierto TRUE
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) => true;
+
+  static LoginBloc of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<Provider>().loginBloc;
+  }
+}
