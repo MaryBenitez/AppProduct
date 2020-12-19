02d58d9e8b8a725efaa5d57ee7cc6c961847596f
Marisol <00094716@uca.edu.sv>
2020-12-18 23:17:56 -0600

    Graba token en el storage del dispositivo

--- lib/src/providers/usuario_provider.dart
+++ lib/src/providers/usuario_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:formvalidation/src/preferencia_usuario/preferencia_usuario.dart';
 import 'package:http/http.dart' as http;
 
 class UsuarioProvider {
@@ -7,6 +8,7 @@ class UsuarioProvider {
     necesito una url particular*/
 
   final String _firebaseToken = 'AIzaSyDD8NqoqSCqdD9ZQOY0XgotpLod5YevtZM';
+  final _prefs = new PreferenciasUsuario();
 
   //Obtengo el token
   Future<Map<String, dynamic>> nuevoUsuario(
@@ -27,7 +29,9 @@ class UsuarioProvider {
     //print(decodedResp);
 
     if (decodedResp.containsKey('idToken')) {
-      //Salvar el token en el storage
+      //Guardo en el dispositivo
+      _prefs.token = decodedResp['idToken'];
+
       return {'ok': true, 'token': decodedResp['idToken']};
     } else {
       return {'ok': false, 'mensaje': decodedResp['error']['message']};
@@ -49,10 +53,11 @@ class UsuarioProvider {
 
     Map<String, dynamic> decodedResp = json.decode(resp.body);
 
-    //print(decodedResp);
-
+    //OBTENGO EL TOKEN
     if (decodedResp.containsKey('idToken')) {
-      //Salvar el token en el storage
+      //Guardo en el dispositivo
+      _prefs.token = decodedResp['idToken'];
+
       return {'ok': true, 'token': decodedResp['idToken']};
     } else {
       return {'ok': false, 'mensaje': decodedResp['error']['message']};
