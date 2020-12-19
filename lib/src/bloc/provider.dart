a13ebdfb2e032c03d15605e3c3bb0e6635f0450f
Marisol <00094716@uca.edu.sv>
2020-12-19 16:56:28 -0600

    Añadiendo productosBloc a inheritedWidget

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:formvalidation/src/bloc/login_bloc.dart';
+import 'package:formvalidation/src/bloc/productos_bloc.dart';
 export 'package:formvalidation/src/bloc/login_bloc.dart';
+export 'package:formvalidation/src/bloc/productos_bloc.dart';
 
 class Provider extends InheritedWidget {
-
   static Provider _instancia;
+
+  final _productosBloc = new ProductosBloc();
 
   factory Provider({Key key, Widget child}) {
     if (_instancia == null) {
@@ -13,10 +16,9 @@ class Provider extends InheritedWidget {
     return _instancia;
   }
 
-  Provider._internal({Key key, Widget child}) 
-    : super(key: key, child: child);
+  Provider._internal({Key key, Widget child}) : super(key: key, child: child);
 
-  final loginBloc = LoginBloc();
+  final loginBloc = new LoginBloc();
 
   //Condicion: Al actualizarse debe notificar a sus hijos. El 99% de los casos es cierto TRUE
   @override
@@ -24,5 +26,12 @@ class Provider extends InheritedWidget {
 
   static LoginBloc of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<Provider>().loginBloc;
+  }
+
+  static ProductosBloc productosBloc(BuildContext context) {
+    //Busca widget llamado "provider"
+    return context
+        .dependOnInheritedWidgetOfExactType<Provider>()
+        ._productosBloc;
   }
 }
